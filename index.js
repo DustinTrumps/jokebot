7a3d01bc4189736f85261a067f456b44f8d3fd84
DustinTrumps <trumpsdustin@outlook.com>
2026-06-20 21:20:57 -0700

    firts night added joke api

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 const { App } = require("@slack/bolt");
 
 const app = new App({
@@ -13,9 +12,47 @@ app.command("/jokebot-ping", async ({ command, ack, respond }) => {
   await ack();
   const latency = Date.now() - start;
   await respond({ text: `Pong!\nLatency: ${latency}ms` });
+  console.log(latency);
+});
+
+app.command("/jokebot-joke-2p", async ({ ack, respond }) => {
+  await ack();
+  const jokeData = await getJOKE('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart');
+  if (!jokeData) {
+    await respond("sorry, I could not get a joke at this time.");
+  } else {
+    await respond({ text: `${jokeData.setup}\n${jokeData.delivery}` });
+  }
+});
+
+app.command("/jokebot-joke-1p", async ({ ack, respond }) => {
+  await ack();
+  const jokeData = await getJOKE('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single');
+  if (!jokeData) {
+    await respond("sorry, I could not get a joke at this time.");
+  } else {
+    await respond({ text: `${jokeData.joke}` });
+  }
 });
 
 (async () => {
   await app.start();
   console.log("bot is running!");
 })();
+
+async function getJOKE(JOKEurl) {
+  try {
+    const response = await fetch(JOKEurl);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Fetch error:', error.message);
+    return null;
+  }
+}
